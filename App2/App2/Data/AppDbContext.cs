e3bfba1145d54d27a99d976a0b47d6bca60c442d
ivost9 <ivostoyanov49@gmail.com>
2024-05-26 14:03:50 +0300

    Initial commit

---
+++ App2/App2/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using App2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App2.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):
+            base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
